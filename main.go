3b4f25f4481bd1e566505f9cc51b3ae4bc4d857e
yupr <12003186+yupr@users.noreply.github.com>
2026-03-30 15:53:47 +0900

    add users api

--- main.go
+++ main.go
@@ -6,11 +6,28 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// NOTE: ユーザーを表す構造体を定義
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
 func main() {
 	r := gin.Default()
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "pong"})
+	})
+
+	r.GET("/users", func(c *gin.Context) {
+		users := []User{
+			{ID: 1, Name: "John", Age: 20},
+			{ID: 2, Name: "Jane", Age: 21},
+			{ID: 3, Name: "Jim", Age: 22},
+		}
+
+		c.JSON(http.StatusOK, users)
 	})
 
 	r.Run(":8080")
